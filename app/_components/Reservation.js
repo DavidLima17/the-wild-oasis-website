b1e2586a37b533554b3c7703faa440da93c8d273
David Lima <David.lima17@gmail.com>
2024-09-16 15:49:59 -0500

    implemented next-auth function with google as provider

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,15 +1,16 @@
+import { auth } from '../_lib/auth';
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
 import DateSelector from './DateSelector';
+import LoginMessage from './LoginMessage';
 import ReservationForm from './ReservationForm';
 
 export default async function Reservation({ cabin }) {
-  // const settings = await getSettings();
-  // const bookedDates = await getBookedDatesByCabinId(params.cabinId);
-
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector
@@ -17,7 +18,12 @@ export default async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from '../_lib/auth';
+
 export const metadata = {
   title: 'Accounts',
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(' ')[0];
+
   return (
     <div>
-      <h1>Accounts</h1>
+      <h1>Welcome, {firstName}!</h1>
     </div>
   );
 }
