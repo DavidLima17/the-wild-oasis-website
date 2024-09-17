9d184c54b947338c1f838a27e9aec965964b70e9
David Lima <David.lima17@gmail.com>
2024-09-16 21:46:39 -0500

    implemented updating reservations by guest with bookings

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,7 +1,7 @@
 'use client';
 
-import { useFormStatus } from 'react-dom';
 import { updateGuest } from '../_lib/actions';
+import SubmitButton from './SubmitButton';
 export default function UpdateProfileForm({ guest, children }) {
   // CHANGE
 
@@ -55,21 +55,10 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating Profile...">
+          Updating Profile
+        </SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? 'Updating...' : 'Update Profile'}
-    </button>
   );
 }
